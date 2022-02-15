c3068ecbb82636c6a9e766f1ac07f8da91568313
giksas <99737436+giksas@users.noreply.github.com>
2022-02-15 15:38:15 +0200

    padirbau sumavima

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 # -*- coding: utf-8 -*-
 # coding: utf-8
 from __future__ import division, print_function
+import sys
 
 def main():
+    print(sys.argv[1:])
+    print(sum(map(int,sys.argv[1:])))
     print("Hello World!", 5/2 == float(5)/2)
 
 if __name__ == "__main__":
