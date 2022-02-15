efeb6dfc2201866d0c07c01ee61c32ff60ed8887
giksas <99737436+giksas@users.noreply.github.com>
2022-02-15 16:22:53 +0200

    maic

--- main.py
+++ main.py
@@ -4,11 +4,32 @@
 from __future__ import division, print_function
 import sys
 
+# Define a function `plus()`
+
+
+def plus(a, b):
+    return a + b
+
+# Create a `Summation` class
+
+    def sum(self, a, b):
+        self.contents = a + b
+        return self.contents
+
+
 def main():
     print(sys.argv[1:])
-    print(sum(map(int,sys.argv[1:])))
-    print()
+    print('sum',sum(map(int, sys.argv[1:])))
+    print('min',min(map(int, sys.argv[1:])))
+    print('max',max(map(int, sys.argv[1:])))
+    print(1, 1, plus(1, 3))
     print("Hello World!", 5/2 == float(5)/2)
+
 
 if __name__ == "__main__":
     main()
+
+
+print('pabaiga')
+
+# & C:/Python27/python.exe c:/Users/Gabrielius/Documents/medis/main.py 1 2 3 4 5 6 7 10 
